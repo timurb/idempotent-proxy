7d527dad159f66524979813108f5e47dddfc1ffc
Timur Batyrshin <erthad@gmail.com>
2017-03-05 20:39:00 +0300

    added ids to storage

--- object_storage.py
+++ object_storage.py
@@ -1,14 +1,40 @@
+import hashlib
+import json
+
+
 class ObjectStorage:
     def __init__(self):
         self.objects = {}
         pass
 
-    def put(self, object_id, object):
-        self.objects[object_id] = object
+    # Put object to storage
+    def put(self, obj):
+        self.objects[self.id_for(obj)] = obj
+        return self.id_for(obj)
 
+    # Get object from storage by id
     def get(self, object_id):
         try:
             result = self.objects[object_id]
         except KeyError:
             result = None
         return result
+
+    # Check if there is object passed as a param in storage
+    def check(self, obj):
+        return self.get(self.id_for(obj))
+
+    # Get id for given object
+    @staticmethod
+    def id_for(obj):
+        return hashlib.sha1(json.dumps(obj, sort_keys=True)).hexdigest()
+
+    # Check if there is object passed as a param in storage
+    def check(self, obj):
+        return self.get(self.id_for(obj))
+
+    # Check if object can be processed.
+    # Dummy method as of now
+    @staticmethod
+    def is_valid(obj):
+        return True
--- tests/test_object_storage.py
+++ tests/test_object_storage.py
@@ -1,12 +1,64 @@
 from object_storage import *
 
 
+def test_id_for():
+    storage = ObjectStorage()
+    assert storage.id_for('foobar') is not None
+    assert storage.id_for(123) is not None
+    assert storage.id_for([123]) is not None
+    assert storage.id_for({'a': 'b'}) is not None
+
+# Empty method as of now
+def test_validate():
+    storage = ObjectStorage()
+    assert storage.is_valid('foo') is True
+    assert storage.is_valid(1) is True
+    assert storage.is_valid([]) is True
+    assert storage.is_valid({}) is True
+
 def test_get_empty():
     storage = ObjectStorage()
     assert storage.get(1) is None
 
 
-def test_set_get():
+def test_check():
     storage = ObjectStorage()
-    storage.put(1, 'foobar')
-    assert storage.get(1) == 'foobar'
+    objid = storage.put('foobar')
+    assert storage.check('foobar') is not None
+    assert storage.check('foobarzed') is None
+
+
+def test_put():
+    storage = ObjectStorage()
+    objid = storage.put('foobar')
+    assert storage.get(objid) == 'foobar'
+
+
+def test_put_uses_object_id():
+    storage = ObjectStorage()
+    storage.put('foobar')
+    objid = storage.id_for('foobar')
+    assert storage.get(objid) == 'foobar'
+
+
+def test_put_doesnt_shadow_each_other():
+    storage = ObjectStorage()
+    storage.put('foobar')
+    storage.put('foobarzed')
+    assert storage.get(storage.id_for('foobar')) == 'foobar'
+    assert storage.get(storage.id_for('foobarzed')) == 'foobarzed'
+
+
+def test_generate_id_for_object_is_not_none():
+    storage = ObjectStorage()
+    assert storage.id_for('foobar') is not None
+
+
+def test_generate_id_for_object_is_unique():
+    storage = ObjectStorage()
+
+    id1 = storage.id_for('foobar')
+    id2 = storage.id_for('foobarz')
+    id3 = storage.id_for('foobar')
+    assert id1 != id2
+    assert id1 == id3
